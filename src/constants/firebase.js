2a3b4850d55e5cc2d40507b754f89b70f068a819
Ben McGraw <ben.mcgraw@gmail.com>
2018-06-11 17:28:19 -0700

    SSO minimally working with Facebook

---
+++ src/constants/firebase.js
@@ -0,0 +1,8 @@
+export default const firebaseConfig = {
+  // ADD YOUR FIREBASE CREDENTIALS
+  apiKey: "AIzaSyCHod7q7yPHC4PjEjouXI2qvqPPOVhWxuU", //web api key
+  authDomain: "",
+  databaseURL: "https://the-magical-cards.firebaseio.com/",
+  projectId: "the-magical-cards",
+  storageBucket: "",
+};
--- src/screens/unauthenticated/Login.js
+++ src/screens/unauthenticated/Login.js
@@ -3,6 +3,14 @@ import React from 'react';
 import { TextInput, View, Button } from 'react-native';
 import firebase from 'react-native-firebase';
 
+// firebase.initializeApp({debug: true})
+
+firebase.auth().onAuthStateChanged(user => {
+  console.log(user);
+});
+
+
+
 export default class Login extends React.Component {
   render() {
     const _facebookLogin = async () => {
@@ -19,6 +27,9 @@ export default class Login extends React.Component {
         // get the access token
         const data = await AccessToken.getCurrentAccessToken();
 
+        console.log("got data");
+        console.log(data);
+
         if (!data) {
           throw new Error('Something went wrong obtaining the users access token'); // Handle this however fits the flow of your app
         }
@@ -26,8 +37,19 @@ export default class Login extends React.Component {
         // create a new firebase credential with the token
         const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
 
+        console.log("got credential");
+        console.log(credential);
+
+        console.log("Let's ask firebase...")
+
         // login with credential
-        await firebase.auth().signInAndRetrieveDataWithCredential(credential);
+        // await firebase.auth().signInAndRetrieveDataWithCredential(credential);
+
+        await firebase.auth().signInAndRetrieveDataWithCredential(credential).catch((error) => {
+          console.log(error)
+        })
+
+        // firebase.auth().signInWithCredential(firebase.auth.FacebookAuthProvider.credential(token))
 
       } catch (e) {
         console.error(e);
